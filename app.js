f699a619cdf229cbf04999bde579f58a0096064a
abdul-00 <abdullah.ali@einaudicorreggio.it>
2022-03-10 22:31:08 +0100

    adding all npm mudules and implemented nav bar in /views/home_page.ejs

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+const mongoose=require('mongoose');
+const app=express();
+const myroutes=require('./routes/home_route');
+require('dotenv/config');
+
+//Middlewere
+app.use(bodyparser.json());
+app.use('/',myroutes);
+
+//set views
+app.set('views','./views');
+app.set('view engine','ejs')
+app.use(express.static('public'));
+
+//DATABASE CONNCETION
+mongoose.connect(process.env.db_connection,{useNewUrlParser: true});
+const db=mongoose.connection;
+db.on('error',()=>console.log("errore di connessione al db"));
+db.once('open',()=>console.log("connesso al db"));
+
+//Start Server
+app.listen(3000);
