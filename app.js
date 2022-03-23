e785e5c82481ff46bc5e4180fda1daa478901fc3
abdul-00 <abdullah.ali@einaudicorreggio.it>
2022-03-23 17:30:27 +0100

    Finished the registration back-end

--- app.js
+++ app.js
@@ -3,24 +3,40 @@ const bodyparser=require('body-parser');
 const mongoose=require('mongoose');
 const app=express();
 const myroutes=require('./routes/home_route');
+const fileupload=require('express-fileupload');
+const session=require('express-session');
+const cookieparser=require('cookie-parser');
+const flash=require('connect-flash');
 require('dotenv/config');
 
-//Middlewere
-app.use(bodyparser.json());
-app.use('/',myroutes);
+
+//session and cookies
+    app.use(cookieparser(process.env.secure_cookie));
+    app.use(session({
+        secret: process.env.session_sec,
+        saveUninitialized: true,
+        resave: true
+    }));
+    app.use(flash());
+    app.use(fileupload());
+
+    // General Middlewere
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended:true}));
+    app.use('/',myroutes);
+
 
 //set views
-app.set('views','./views');
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
+    app.set('views','./views');
+    app.set('view engine','ejs');
+    app.use(express.urlencoded({extended:true}));
+    app.use(express.static('public'));
 
 //DATABASE CONNCETION
-/*
-mongoose.connect(process.env.db_connection,{useNewUrlParser: true});
-const db=mongoose.connection;
-db.on('error',()=>console.log("errore di connessione al db"));
-db.once('open',()=>console.log("connesso al db"));*/
+    mongoose.connect(process.env.db_connection,{useNewUrlParser: true});
+    const db=mongoose.connection;
+    db.on('error',()=>console.log("errore di connessione al db"));
+    db.once('open',()=>console.log("connesso al db"));
 
 //Start Server
-app.listen(3000);
+    app.listen(3000);
--- routes/home_route.js
+++ routes/home_route.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require('mongoose');
 const router=express.Router();
 const myuser=require('../models/user');
 const myproduct=require('../models/prodotto');
-
+const bcrypte=require('bcryptjs');
 //MAIN ROOT
 router.get('/', (req, res) =>{
     res.render('home_page');
@@ -21,15 +21,52 @@ router.get('/login' ,(req,res) =>{
 });
 
 //Registration root
-router.post('/register',(req,res)=>{
-    //WORKING ON THIS FOR NOW
-    var registration = new myuser({ 'nome': 'alex','indirizzo':'via mario 25','email':'alex.b@gmail.com'});
-    // Save the new model instance, passing a callback
-    awesome_instance.save(function (err) {
-    if (err) return handleError(err);
-        res.send("controlla mongodb atlas");
+    router.get('/register',(req,res)=>{
+        const infoErrorObj= req.flash('infoError');
+        const infoSubmitObj= req.flash('infoSubmit');
+        res.render('register',{infoErrorObj,infoSubmitObj});
     });
-});
+
+    router.post('/register', async (req,res)=>{
+        var result=req.body;
+        //verificare i dati
+        if(result.email[0]!=result.email[1] || result.password[0]!=result.password[1] || result.codice_fiscale.lenght>16 || result.codice_fiscale.lenght<16 ){
+            req.flash('infoError'," Errore di compilazione");
+            res.redirect('/register');
+            return;
+        };
+        //query se esite gia un utente con la mail inserita o codicefiscale
+        myuser.findOne({ $or:[{'email':result.email[0]},{'codicefiscale':result.codice_fiscale}] },async function (err, myres) {
+                if (err) return handleError(err);
+                console.log(myres);
+                if (myres) {
+                    req.flash('infoError'," Errore di compilazione");
+                    res.redirect('/register');
+                    return;
+                }
+                console.log("okay");
+                req.flash('infoSubmit','Registrazione Completata');
+                res.redirect('/register');
+                //hash the password
+                bcrypte.hash(result.password[0], 10 , function(err, hash) {
+                    //save data to db
+                    var registration= new myuser({
+                        nome: result.nome,
+                        cognome: result.cognome,
+                        data_nascita: result.data_nascita,
+                        codicefiscale: result.codice_fiscale,
+                        email: result.email[0],
+                        password: hash,
+                    });
+                    registration.save();
+                });
+
+            })
+
+        //creare il token per l'accesso
+        //mandare email di avvenuta registraione <-----Opzionale
+
+    });
 
 //General Porpuse
 router.get('/find',(req,res)=>{
