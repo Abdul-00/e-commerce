e18d808df21ce401c13b3eb28f111fe691f34fca
unknown <alexbattiston0703@gmail.com>
2022-04-06 09:54:37 +0200

    fixed bug javascript for delete account

--- public/javascript/delete_account.js
+++ public/javascript/delete_account.js
@@ -1,11 +1,7 @@
-'use strict';
-
 function EnableDisableTextBox(drop_down_motivazione) {
-    let selectedValue = drop_down_motivazione.options[drop_down_motivazione.selectedIndex].value;
-    let text_box_motivazione_altro = document.getElementById("text_box_motivazione_altro");
-    if (selectedValue !== "Altro") {
-        text_box_motivazione_altro.style.display = "none";
-    }else{
-        text_box_motivazione_altro.style.display = "block";
+    if (drop_down_motivazione=="altro"){
+        document.getElementById("text_box_motivazione_altro").style.display = "block";
+    }  else{
+        document.getElementById("text_box_motivazione_altro").style.display = "none";
     }
 }
